3313713ba5054531fff858d66f12adca5f3d7934
Edi94bravo <114755092+RugalMc@users.noreply.github.com>
2023-11-18 22:38:32 -0500

    script.js

--- Retos javascript/script.js
+++ Retos javascript/script.js
@@ -14,3 +14,11 @@ console.log(`El mensaje de ${message} tiene ${message.length} caracteres`)
 console.log(`El tercer número del array con los valores ${array} es: ${array[2]}`)
 console.log(`Se añade el número que sigue en orden a final del arreglo y es: ${array[array.length - 1]}`)
 console.log(`Se encontró el patron '${word}' en el texto ${texto}, por lo que el resultado es: ${word}`)
+
+¡Hola mundo! Soy Edison
+Mi nombre en mayúsculas es: EDISON
+Mi nombre en minúsculas es: edison
+El mensaje de ¡Hola mundo! tiene 12 caracteres
+El tercer número del array con los valores 1,2,3,4,5,6 es: 3
+Se añade el número que sigue en orden a final del arreglo y es: 6
+Se encontró el patron 'lluvia' en el texto La lluvia en Sevilla es una maravilla, por lo que el resultado es: lluvia
